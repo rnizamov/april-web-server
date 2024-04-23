9fbb685cf3d6b00a46518a61214f9013efebcdb6
Rinat.Nizamov <_megion_@mail.ru>
2024-04-23 03:42:21 +0300

    добавил картинку вместо текста 404

--- src/main/java/ru/rnizamov/web/server/processors/UnknownOperationRequestProcessor.java
+++ src/main/java/ru/rnizamov/web/server/processors/UnknownOperationRequestProcessor.java
@@ -9,7 +9,8 @@ import java.nio.charset.StandardCharsets;
 public class UnknownOperationRequestProcessor implements RequestProcessor {
     @Override
     public void execute(HttpRequest httpRequest, OutputStream output) throws IOException {
-        String response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<html><body><h1>NOT FOUND</h1></body></html>";
+        String response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<html><body>" +
+                "<img src='https://l-tech.ru/uploads/publication_post/main_image/122/full_screen_retina_e5f347c8e4.jpeg' style='max-width: 100%; height: auto;'></body></html>";
         output.write(response.getBytes(StandardCharsets.UTF_8));
     }
 }
